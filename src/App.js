68b121b6880775f7485cee10a9960bbde7a3aa12
Akshay Chandwani <akshaychandwani992@gmail.com>
2021-08-19 20:22:36 +0530

    try deploy

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React deplyed from amplify
         </a>
       </header>
     </div>
